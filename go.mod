38455062cf609db54e9300cfd6f8a7699ebcb993
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-02-10 01:46:51 -0500

    DFA for binary multiples of 3

---
+++ go.mod
@@ -0,0 +1,5 @@
+module binarymultipleof3
+
+go 1.21.5
+
+require github.com/0721santi/DFAAnalyzer v0.0.0-20240210062421-7063c8ed9f7f
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	dfaanalyzer "github.com/0721santi/DFAAnalyzer"
+)
+
+func main() {
+	dfa := dfaanalyzer.GenDFA()
+	dfa.AddSymbol("0")
+	dfa.AddSymbol("1")
+	q0, _ := dfa.AddState("0", true)
+	q1, _ := dfa.AddState("1", false)
+	q2, _ := dfa.AddState("2", false)
+	dfa.SetStartState("0")
+	dfa.AddTransitionFunc(q0, q0, []string{"0"})
+	dfa.AddTransitionFunc(q0, q1, []string{"1"})
+	dfa.AddTransitionFunc(q1, q0, []string{"1"})
+	dfa.AddTransitionFunc(q1, q2, []string{"0"})
+	dfa.AddTransitionFunc(q2, q1, []string{"0"})
+	dfa.AddTransitionFunc(q2, q2, []string{"1"})
+	var str string
+	fmt.Scanln(&str)
+	accepted := dfa.AnalizeChain(str)
+	if accepted {
+		fmt.Printf("The DFA accepts the string %s\n", str)
+	} else {
+		fmt.Printf("The DFA rejects the string %s\n", str)
+	}
+}
